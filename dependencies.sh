0c74ae229d9fbd97f42a35e3be797be27cfb3ff7
thiennp3 <thiennp3@vng.com.vn>
2026-06-07 15:12:00 +0700

    update gitignore and dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,3 @@
 conda activate thesis
-conda install pytorch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 cpuonly -c pytorch
+conda install pytorch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install -r requirements.txt
